cdc5181e668abe5bfef496a1bf3a36adfccd8acc
Chris Garretty <cgarretty@gmail.com>
2020-12-09 12:57:07 -0500

    add day 2 with steps.

---
+++ day2/PasswordChecker.java
@@ -0,0 +1,12 @@
+// TODO:
+
+// Create password object that takes password String
+// create a method to parse the password.
+// create a method that checks the password against it's policy.
+
+// Create a passwords object
+// create a method to read passwords from txt file.
+
+// create main program loop
+// 1. read in passwords from file
+// 2. check each password, and keep a tally of valid and invalid passwords.
