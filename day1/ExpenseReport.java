1a1416af002afb0b3fedf4127a9a624014d5013f
Chris Garretty <cgarretty@gmail.com>
2020-12-09 11:23:34 -0500

    day 1, solution 2! needs some clean-up

--- day1/ExpenseReport.java
+++ day1/ExpenseReport.java
@@ -6,34 +6,21 @@ import java.util.Scanner;
 
 public class ExpenseReport {
    public static void main(String args[]) throws Exception {
-     int i;
-     int j;
-     int x;
-     int y;
-     int [] expenses = new Expenses().ReadExpenses();
-
-     // accessing each element of array
-     for (i = 0; i < expenses.length; i++) {
-            x = expenses[i];
-            for (j = 0; j < expenses.length; j++) {
-              if (i != j) {
-                y = expenses[j];
-                if (x + y == 2020) {
-                  System.out.println("x:" + x);
-                  System.out.println("y:" + y);
-                  System.out.println("Solution:" + (x*y));
-                  System.exit(0);
-                }
-              }
-            }
-     };
+     int solveFor = 3;
+     Expenses expenses = new Expenses();
+     expenses.ReadExpenses("expenses.txt");
+     int [] counters = new int [solveFor];
+     expenses.Solve(0, counters);
    }
+
 }
 
 class Expenses {
+  public int [] expenses;
+  public int [] counters;
 
-  int [] ReadExpenses () throws Exception {
-    Scanner scanner = new Scanner(new FileReader("expenses.txt"));
+  void ReadExpenses (String filePath) throws Exception {
+    Scanner scanner = new Scanner(new FileReader(filePath));
     int [] expenses = new int [200];
     int i = 0;
 
@@ -41,6 +28,56 @@ class Expenses {
         expenses[i++] = scanner.nextInt();
     }
 
-    return expenses;
+    this.expenses = expenses;
   }
-}
+
+    static int Sum(int [] arr) {
+      int i;
+      int sum = 0;
+
+      for (i = 0; i < arr.length; i++)
+        sum +=  arr[i];
+
+      return sum;
+    }
+
+    static int Mult(int [] arr) {
+       int i;
+       int prod = arr[0];
+
+       for (i = 1; i < arr.length; i++)
+          prod = prod * arr[i];
+
+       return prod;
+    }
+
+    int [] MakeArray(int [] counters) {
+      int [] arr = new int[counters.length];
+      for (int i=0; i < counters.length; i++) {
+        arr[i] = this.expenses[counters[i]];
+      }
+      return arr;
+    }
+
+    void Check(int [] counters) {
+      int [] arr = MakeArray(counters);
+        if (Sum(arr) == 2020) {
+            System.out.println("Counters: " + Arrays.toString(counters));
+            System.out.println("Array: " + Arrays.toString(arr));
+            System.out.print("Solution:" + Mult(arr));
+            System.exit(0); // this ends the program.
+        }
+    }
+
+    void Solve(int level, int [] counters) {
+      int length = this.expenses.length;
+      if(level == counters.length) {
+        Check(counters);
+      }
+      else {
+        for (counters[level] = 0; counters[level] < length; counters[level]++) {
+            Solve(level + 1, counters);
+        }
+      }
+    }
+  }
