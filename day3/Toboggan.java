71cc822bfc93ecf07e77e10c002f928bda1aaf2e
Chris Garretty <cgarretty@gmail.com>
2020-12-10 19:10:51 -0500

    solution 2!

--- day3/Toboggan.java
+++ day3/Toboggan.java
@@ -5,17 +5,32 @@ import java.util.Scanner;
 class Toboggan {
   public static void main(String args[]) throws Exception {
     Map map = new Map("map.txt");
-    Sled sled = new Sled();
-    int moves = map.length - 1;
-    int n = 0;
-    for (int i = 0; i < moves; i++) {
-      sled.move(3, 1);
-      if (map.isTree(sled.x, sled.y)) {
-        n++;
+    int[] s = new int[5];
+    int[][] slopes = {{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}};
+    for (int k = 0; k < slopes.length; k++) {
+      Sled sled = new Sled();
+      int n = 0;
+      while (sled.y < map.length - 1) {
+        sled.move(slopes[k][0], slopes[k][1]);
+        if (map.isTree(sled.x, sled.y)) {
+          n++;
+        }
       }
+      s[k] = n;
+      System.out.println("Slope: (" + slopes[k][0] + ", " + slopes[k][1] + ")");
+      System.out.println("Trees Hit: " + n);
     }
-    System.out.println("Moves Made: " + map.length);
-    System.out.println("Tree Hit: " + n);
+    System.out.println("Solution: " + mult(s));
+    System.out.println();
+  }
+
+  static long mult(int[] arr) {
+    long prod = 1;
+    for (int i = 0; i < arr.length; i++) {
+      prod = prod * arr[i];
+    }
+
+    return prod;
   }
 }
 
@@ -33,6 +48,9 @@ class Map {
   }
 
   boolean isTree(int x, int y) {
+    if (y > this.length) {
+      return false;
+    }
     Row row = this.rows.get(y);
     if (row.get(x) == '#') {
       return true;
