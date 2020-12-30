8cc27543dc0980904079a33dec34e642e4f8b087
Chris Garretty <cgarretty@gmail.com>
2020-12-29 22:35:00 -0800

    get it started!

---
+++ day4/PassportChecker.java
@@ -0,0 +1,13 @@
+// Create the main program loop
+  // 1. create passwords from file
+  // 2. count valid passwords
+  // 3. print result
+
+// Create Passwords object
+  // 1. create attributes for valid count & passwords.
+  // 2. method for counting valid passwords
+
+// Create Passport object
+ // 1. make attributes for each field, with default null.
+ // 2. validate method that returns true if valid.
+ // 3. parse attribute method
